47aa92b65ee06d6d046c74c6de709f0fe32fb110
Drew Grey <dtsgrey@gmail.com>
2022-02-09 14:05:00 -0600

    commit tree ec56fa64af3870cbe7ee09706553ba658c9deb84

--- src/index.js
+++ src/index.js
@@ -1,1 +1,67 @@
-// write your code here
+document.addEventListener("DOMContentLoaded",function(){  //loads your js into the browser
+    function fetchRamens(){
+        fetch("http://localhost:3000/ramens")
+        .then(response => response.json() )
+        .then(data => renderRamens(data))
+    }
+
+    const ramenMenu = document.getElementById('ramen-menu')
+    const newRamenForm = document.querySelector('#new-ramen')
+
+    newRamenForm.addEventListener("submit",function(event){
+        event.preventDefault();
+        const newRamenName = document.querySelector("#new-name").value
+        const newRamenRestaurant = document.querySelector("#new-restaurant").value
+        const newRamenImage = document.querySelector("#new-image").value
+        const newRamenRating = document.querySelector("#new-rating").value
+        const newRamenComment = document.querySelector("#new-comment").value 
+        let newRamen = {
+            id: 6, 
+            name: newRamenName,
+            restaurant: newRamenRestaurant,
+            image: newRamenImage,
+            rating: newRamenRating,
+            comment: newRamenComment,
+        };
+        const newImageElem = document.createElement("img")
+        newImageElem.setAttribute("src",newRamen.image)
+        newImageElem.setAttribute("id",newRamen.id)
+        newImageElem.addEventListener("click", function(){
+            renderRamen(newRamen)
+        })
+        ramenMenu.appendChild(newImageElem)
+        newRamenForm.reset()
+    })
+
+    function renderRamens(ramens){
+        ramens.forEach(ramen => { //iterates through ramen and sets new html element named img and then sets the "src" atrribute for the images
+            const ramenImage = document.createElement("img") 
+            ramenImage.setAttribute("src", ramen.image )
+            ramenImage.setAttribute("id", ramen.id)
+            ramenImage.addEventListener("click", function(){
+                fetchRamen(ramenImage)
+            })
+            ramenMenu.appendChild(ramenImage)
+        })
+    }
+    
+    function fetchRamen(ramen){
+        fetch(`http://localhost:3000/ramens/${ramen.id}`)
+        .then(response => response.json())
+        .then(data => renderRamen(data))
+    }
+
+    function renderRamen(ramen){
+        const ramenImage = document.querySelector('.detail-image')
+        ramenImage.setAttribute("src",ramen.image)
+        const ramenName = document.querySelector('.name')
+        ramenName.innerText = ramen.name
+        const ramenRestaurant = document.querySelector('.restaurant')
+        ramenRestaurant.innerText = ramen.restaurant
+        const ramenRating = document.querySelector('#rating-display')
+        ramenRating.innerText = ramen.rating
+        const ramenComment = document.querySelector('#comment-display')
+        ramenComment.innerText = ramen.comment
+    }
+    fetchRamens();
+});
